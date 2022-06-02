b9bdb64fefe7605bb88da94d517c30f8ba8d9e27
Tyler Gandy <tylerbgandy@gmail.com>
2022-06-02 16:08:09 -0400

    changes made to header

--- cash-flow-investments/src/components/header/Header.js
+++ cash-flow-investments/src/components/header/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { RiMoneyPoundBoxLine } from "react-icons/ri";
+import "./Header.css";
+import { BiSearch } from "react-icons/bi";
 
 function Header() {
   return (
@@ -10,6 +12,10 @@ function Header() {
           src="https://user-images.githubusercontent.com/94323045/170417711-03fa3ac9-ee41-450b-9a91-a14af811952a.jpg"
           alt=""
         ></img>
+      </div>
+      <div className="header__center">
+        <input type="text" />
+        <BiSearch />
       </div>
     </div>
   );
