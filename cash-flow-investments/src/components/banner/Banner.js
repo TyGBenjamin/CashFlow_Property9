305d44b123c2da5ebe62310ebb1a11ba96ea55ba
Tyler Gandy <tylerbgandy@gmail.com>
2022-06-21 23:56:33 -0400

    changes made to homepage

--- cash-flow-investments/src/components/banner/Banner.js
+++ cash-flow-investments/src/components/banner/Banner.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./Banner.css";
 
 function Banner() {
-  return <div className="banner">Banner</div>;
+  return (
+    <div className="banner">
+      <div className="banner__info">
+        <h1> Get out and stretch your imagination</h1>
+        <h5>
+          {" "}
+          Plan a different type of getaway to uncover the hidden gems near you.
+        </h5>
+        <button className="bannerButton"> Explore Nearby</button>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
